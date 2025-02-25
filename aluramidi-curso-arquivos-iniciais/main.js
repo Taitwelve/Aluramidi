a50e4464f5c3c749ecd6bbc86e2ed8e4b247cde5
taitwelve <taynascimento15@gmail.com>
2025-02-25 18:40:08 -0300

    fix: textos dinâmicos

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,7 +8,14 @@ let contador = 0;
 
 //enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    // console.log(instrumento);
+    const idAudio = `#som_${instrumento}`;
+    //  console.log(idAudio);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
-    console.log(contador);
+    // console.log(contador);
 }
